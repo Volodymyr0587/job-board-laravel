4afb874b0aa21037fd8ede6aef1bef57853d609b
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-09-24 08:39:10 +0300

    apply listing

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -26,7 +26,7 @@
                             <strong>Location: </strong>{{ $listing->location }}<br>
                             <strong>Company: </strong>{{ $listing->company }}
                         </p>
-                        <a href="#" class="block text-center my-4 tracking-wide bg-white text-indigo-500 text-sm font-medium title-font py-2 border border-indigo-500 hover:bg-indigo-500 hover:text-white uppercase">Apply Now</a>
+                        <a href="{{ route('listings.apply', $listing->slug) }}" class="block text-center my-4 tracking-wide bg-white text-indigo-500 text-sm font-medium title-font py-2 border border-indigo-500 hover:bg-indigo-500 hover:text-white uppercase">Apply Now</a>
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::get('/{listing}', [ListingController::class, 'show'])->name('listings.show');
+Route::get('/{listing}/apply', [ListingController::class, 'apply'])->name('listings.apply');
